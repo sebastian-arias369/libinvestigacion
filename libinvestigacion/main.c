9a3e9ee388688d1cbee66216281e9fdd4293d897
Sebastian Arias <juan.arias06@uceva.edu.co>
2023-11-24 00:02:01 -0500

    se creo el metodo del arreglo de cursos que sirve para copiar informacion de los atributos de los cursos con su inicio de horario y su finalizacion

--- libinvestigacion/main.c
+++ libinvestigacion/main.c
@@ -53,3 +53,47 @@ int main() {
         salas[i].horario_asignado.inicio = 0;
         salas[i].horario_asignado.fin = 0;
     }
+
+    Curso cursos[NUM_CURSOS];
+
+        strcpy(cursos[0].nombre, "Arquitectura");
+        cursos[0].num_estudiantes = 14;
+        strcpy(cursos[0].profesor, "Alvaro Salazar");
+         cursos[0].horario_curso.inicio = 840;
+        cursos[0].horario_curso.fin = 1000;
+
+
+        strcpy(cursos[1].nombre, "Analisis Númerico");
+        cursos[1].num_estudiantes = 19;
+        strcpy(cursos[1].profesor, "Efrain Vazques");
+         cursos[1].horario_curso.inicio = 1000;
+        cursos[1].horario_curso.fin = 1140;
+
+
+        strcpy(cursos[2].nombre, "Discretas l");
+        cursos[2].num_estudiantes = 24;
+        strcpy(cursos[2].profesor, "ANDRES FERNANDO");
+        cursos[2].horario_curso.inicio = 1200;
+        cursos[2].horario_curso.fin = 1340;
+
+        strcpy(cursos[3].nombre, "WEB ll");
+        cursos[3].num_estudiantes = 24;
+        strcpy(cursos[3].profesor, "Álvaro Salazar");
+        cursos[3].horario_curso.inicio = 1400;
+        cursos[3].horario_curso.fin = 1540;
+
+        strcpy(cursos[4].nombre, "InvestigacionOp");
+        cursos[4].num_estudiantes = 24;
+        strcpy(cursos[4].profesor, "Giovanny Cruz");
+        cursos[4].horario_curso.inicio = 1600;
+        cursos[4].horario_curso.fin = 1740;
+
+
+        strcpy(cursos[5].nombre, "Sistemas Operativos");
+        cursos[5].num_estudiantes = 26;
+        strcpy(cursos[5].profesor, "Wilmar Salgado");
+        cursos[5].horario_curso.inicio = 1800;
+        cursos[5].horario_curso.fin = 1940;
+        asignarCursosASalas(cursos, salas);
+
+        asignarCursosASalas(cursos, salas);
