f77988a59ae117d4c228ab874dc9acc87b49b756
Sebasjaga <sebastian.jaramillo03@uceva.edu.co>
2023-11-23 23:38:00 -0500

    Se implementó la clase asignacion.c con el método de asignarCursosASalas que asigna el horario de los cursos y de las salas correspondientes

--- libinvestigacion/asignacion.c
+++ libinvestigacion/asignacion.c
@@ -1,6 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include "asignacion.h"
+#include <string.h>
 
+void asignarCursosASalas(Curso cursos[], Sala salas[]) {
+    for (int i = 0; i < NUM_CURSOS; i++) {
+        for (int j = 0; j < NUM_SALAS; j++) {
+            if (cursos[i].num_estudiantes <= salas[j].capacidad) {
+                Horario horario_curso = cursos[i].horario_curso;
+                Horario horario_sala = salas[j].horario_asignado;
+                if ((horario_curso.inicio >= horario_sala.fin || horario_curso.fin <= horario_sala.inicio) && salas[j].horario_asignado.inicio == 0) {
+                    cursos[i].asignada = 1;
+                    strcpy(cursos[i].asignada_sala, salas[j].nombre);
+                    salas[j].horario_asignado = cursos[i].horario_curso;
+                    break;
+                }
+            }
+        }
+    }
+}
